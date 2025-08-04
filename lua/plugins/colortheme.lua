cee5a2488324cea49675be0382f3facc1f7f92fd
Zakky <150591719+zakky20@users.noreply.github.com>
2025-08-04 18:37:14 +0300

    Add files via upload

---
+++ lua/plugins/colortheme.lua
@@ -0,0 +1,37 @@
+return {
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		-- Set initial background transparency
+		local bg_transparent = true
+
+		-- Configure the tokyonight style
+		require("tokyonight").setup({
+			style = "storm", -- or "night", "moon", "day"
+			transparent = bg_transparent,
+			styles = {
+				comments = { italic = false },
+				keywords = { italic = false },
+				functions = {},
+				variables = {},
+				sidebars = "dark",
+				floats = "dark",
+			},
+		})
+
+		-- Load the colorscheme
+		vim.cmd([[colorscheme tokyonight]])
+
+		-- Toggle transparency
+		local toggle_transparency = function()
+			bg_transparent = not bg_transparent
+			require("tokyonight").setup({
+				transparent = bg_transparent,
+			})
+			vim.cmd([[colorscheme tokyonight]])
+		end
+
+		vim.keymap.set("n", "<leader>bg", toggle_transparency, { noremap = true, silent = true })
+	end,
+}
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,69 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		local mode = {
+			"mode",
+			fmt = function(str)
+				return "" .. str
+			end,
+		}
+
+		local filename = {
+			"filename",
+			file_status = true, -- displays file status (readonly status, modified status)
+			path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
+		}
+
+		local hide_in_width = function()
+			return vim.fn.winwidth(0) > 100
+		end
+
+		local diagnostics = {
+			"diagnostics",
+			sources = { "nvim_diagnostic" },
+			sections = { "error", "warn" },
+			symbols = { error = " ", warn = " ", info = " ", hint = " " },
+			colored = false,
+			update_in_insert = false,
+			always_visible = false,
+			cond = hide_in_width,
+		}
+
+		local diff = {
+			"diff",
+			colored = false,
+			symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+			cond = hide_in_width,
+		}
+
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				Theme = "nord",
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				disabled_filetypes = { "alpha", "neo-tree" },
+				always_divide_middle = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = { "filename" },
+				lualine_x = { "fileformat", "filetype", "encoding" },
+				lualine_y = { "location" },
+				lualine_z = { "progress" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filetype", "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			extensions = { "fugitive" },
+		})
+	end,
+}
